5d50d9b3d3c8fc241a8b5aa892185237d3ac033a
EmperorDa8 <uabdul88@gmail.com>
2022-07-31 08:27:22 -0700

    made some modification to them

--- Findin/account/form.py
+++ Findin/account/form.py
@@ -3,11 +3,28 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
+clienttype=(
+    ("0",""),
+    ("1","company"),
+    ("2","investor"),
+    ("3","idealist"),
+
+)
+
+sex=(
+    ("0",""),
+    ("1","female"),
+    ("2","male"),
+)
+
 class registerform(UserCreationForm):
     email=forms.EmailField()
     country=forms.Field()
     age=forms.IntegerField()
-
+    Firstname=forms.TextInput()
+    lastername=forms.TextInput()
+    user_type=forms.ChoiceField(choices=clienttype)
+    gender=forms.ChoiceField(choices=sex)
     class meta:
         model = User
         fields = [
@@ -16,8 +33,7 @@ class registerform(UserCreationForm):
             "lastname",
             "age",
             "email",
-            "password1"
-            "password2"
-            "User-type",
+            "password",
+            "User_type",
             "gender",
         ]
